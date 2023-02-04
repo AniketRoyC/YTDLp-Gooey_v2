f48a97bc6502675cfb8f442202999c743d1feeaf
AniketRoyC <aniket.roychow@gmail.com>
2023-02-04 13:58:53 -0500

    added gitignore, more stuff

--- gooey_v2_main.py
+++ gooey_v2_main.py
@@ -1,3 +1,4 @@
+#IMPORTS==================================================================
 import yt_dlp
 #https://github.com/yt-dlp/yt-dlp#embedding-yt-dlp
 # ℹ️ See help(yt_dlp.YoutubeDL) for a list of available options and public functions
@@ -10,6 +11,57 @@ from tkinter import ttk
 from datetime import datetime
 print(datetime.now()) #going to use this to track time spent downloading.
 
+#=========================================================================
+#Logger for intercepting output:
+class MyLogger(object):
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        print(msg)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Done downloading, now converting ...')
+
+##=========================================================================
+vibecheck = {
+    'key': 'FFmpegExtractAudio',
+    'preferredcodec': 'mp3',
+    'preferredquality': '192',
+}
+
+
+
+# https://github.com/yt-dlp/yt-dlp/blob/master/yt_dlp/YoutubeDL.py#L180
+ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [],
+    'logger' : MyLogger(),
+    'progress_hooks' : [my_hook],
+}
+
+
+
+
+
+def run_ytdl():
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        # info = ydl.extract_info(URL, download=False)
+
+        # # ℹ️ ydl.sanitize_info makes the info json-serializable
+        # print(json.dumps(ydl.sanitize_info(info)))
+        ydl.download([URL])
+
+def XAUD_changed():
+    ydl_opts['postprocessors'] = vibecheck
+
+
+
 root = Tk() #base window
 root.title('Gooey_v2 - Main')
 
@@ -17,42 +69,37 @@ mainframe = ttk.Frame(root, padding = '3 3 12 12') #main frame in the 'root' win
 mainframe.grid(column = 0, row = 0, sticky=(N,W,E,S)) #grid within the main frame
 
 #expand the frame to fill any extra space on re-size:
-root.columnconfigure(0, weight = 1)
+root.columnconfigure([1,2], weight = 1)
 root.rowconfigure(0, weight = 1)
-label = Label(text="Hello, Tkinter")
+
+
 
 
 
 #INPUT - URL Entry
-URL_label = ttk.Label(mainframe, text = "Video URL")
-URL_label.grid(column = 1, row = 1, sticky = W, padx = 5, pady = 5)
+URL_label = ttk.Label(mainframe, text = 'Video URL:')
+URL_label.grid(column = 1, row = 1, sticky = W)
 
 URL = StringVar()
 URL_entry = ttk.Entry(mainframe, width = 25, textvariable = URL)
-URL_entry.grid(column = 2, row = 1, sticky = (W,E), padx = 5, pady = 5)
-
+URL_entry.grid(column = 2, row = 1, sticky = (W,E))
 URL_entry.focus()
 
+#EXTRACT AUDIO - Checkbox for Audio Only
+XAUD = StringVar()
+XAUD_check = ttk.Checkbutton(mainframe, text = 'Audio Only',
+                            variable = XAUD, command=XAUD_changed,
+                            onvalue = 'XAUD_on', offvalue = 'XAUD_off')
+XAUD_check.grid(column = 3,row = 1, sticky = E)
 
-root.mainloop()
+
+#RUN - Button to run ytdlp
+RUN_button = ttk.Button(mainframe, text = 'Run ytdlp', command = run_ytdl)
+RUN_button.grid(column = 3, row = 3, sticky = W)
 
 
 #Padding every widget at once (if no custom padding is needed)
-# for child in mainframe.winfo_children(): 
-#     child.grid_configure(padx=5, pady=5)
+for child in mainframe.winfo_children(): 
+    child.grid_configure(padx=5, pady=5)
 
-
-
-# YOUTUBE DL STUFF, DEAL WITH THIS ONCE THE GUI EXISTS
-
-
-# # https://github.com/yt-dlp/yt-dlp/blob/master/yt_dlp/YoutubeDL.py#L180
-# ydl_opts = {
-
-
-# }
-# with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-#     info = ydl.extract_info(URL, download=False)
-
-#     # ℹ️ ydl.sanitize_info makes the info json-serializable
-#     print(json.dumps(ydl.sanitize_info(info)))
+root.mainloop()
